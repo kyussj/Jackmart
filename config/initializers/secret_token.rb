97d778adfcb87fe392f8a11b0e32bc4f2c1fe26f
Jack Booth <j.booth.07@aberdeen.ac.uk>
2012-04-26 15:06:27 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jackmart::Application.config.secret_token = '35ea621cbb2157e2307e5afadfec26c0dfb1f971add7b187670abad076a2e7b8a897de8bf6233fd3adf111707b8db9eca4e37bdd1386e29a5d49ef2c2e55bd35'
