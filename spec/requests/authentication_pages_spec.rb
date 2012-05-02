0291724ff95ec4101aa94edf8bdc2553f2bb88a9
Jack Booth <j.booth.07@aberdeen.ac.uk>
2012-05-02 12:10:49 +0100

    fixed broken test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,7 +35,7 @@ describe "Authentication" do
       end
 
       it { should have_selector('title', text: user.name) }
-      it { should have_link('Products', href: user_path(user)) }
+      it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
