a5308300ebb5309981d5aec0b089aae73086d24a
Jack Booth <j.booth.07@aberdeen.ac.uk>
2012-05-07 15:07:24 +0100

    fixing email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-	@url  = "https://smooth-mist-5552.herokuapp.com/"
-    mail(:to => user.email, :subject => "Welcome to My Awesome Site")
+	@url  = "https://jackmart.herokuapp.com/"
+    mail(:to => user.email, :subject => "Welcome to the Jackmart app")
   end
 end
